c022d47aa6a24d83e7c30c8ec26e29cb6eb8310f
Macederkasd <patt_002@users.noreply.github.com>
2019-05-21 21:15:22 -0700

    First Web

---
+++ javascript.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    // Initialize Tooltip
+    $('[data-toggle="tooltip"]').tooltip(); 
+})
+
+$(document).ready(function(){
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 900, function(){
+        window.location.hash = hash;
+      });
+    }  
+  });
+})
